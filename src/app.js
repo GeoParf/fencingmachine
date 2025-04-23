703835d121a047cc9aae6452aa4239f258adfc98
GeoParf <theoak@list.ru>
2025-04-23 13:16:53 -0400

    URLfix

--- src/app.js
+++ src/app.js
@@ -126,10 +126,10 @@ function optionTaggler(){
   isOptionVisible = !isOptionVisible;
     if(isOptionVisible){
       optionsList.style.visibility = "visible";
-      optionsBtn.children[0].src = "../../src/assets/icons/settings.png";
+      optionsBtn.style.backgroundImage = "url(../src/assets/icons/close.png)" ;
     } else {
       optionsList.style.visibility = "hidden";
-      optionsBtn.children[0].src = "../../src/assets/icons/settings.png";
+      optionsBtn.style.backgroundImage  = "url(../src/assets/icons/settings.png)";
   }
 };
 
@@ -137,10 +137,10 @@ function instructionTaggler(){
   isInstructionVisible = !isInstructionVisible;
   if(isInstructionVisible ){
     instruction.style.visibility = "visible";
-    instructionBtn.children[0].src = "../src/assets/icons/close.png";
+    instructionBtn.style.backgroundImage = "url(../src/assets/icons/close.png)";
   } else {
     instruction.style.visibility = "hidden";
-    instructionBtn.children[0].src = "../src/assets/icons/question-mark-icon.png";
+    instructionBtn.style.backgroundImage = "url(../src/assets/icons/question-mark-icon.png)";
   }
 };
 
